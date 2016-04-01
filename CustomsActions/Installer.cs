79b6fdf0671f0930547c847bad12d8ddcad9bb7e
kjrcda <kjrcda@hotmail.com>
2016-04-01 01:50:51 -0500

    Add custom action to installer
    This will now remove all saved files created by the application

---
+++ CustomsActions/Installer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.IO;
+
+namespace CustomsActions
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Finance Tracker");
+            if(Directory.Exists(path))
+                Directory.Delete(path, true);
+        }
+    }
+}
