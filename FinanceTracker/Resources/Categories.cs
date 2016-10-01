b965995e928eb24b6cae0e329ddc27a6d17edbfd
kjrcda <kjrcda@hotmail.com>
2016-10-01 13:22:01 -0500

    use reflection and properties to get info about categories more efficient

--- FinanceTracker/Resources/Categories.cs
+++ FinanceTracker/Resources/Categories.cs
@@ -1,4 +1,8 @@
-﻿namespace FinanceTracker.Resources
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace FinanceTracker.Resources
 {
     public static class Categories
     {
@@ -10,19 +14,36 @@
         public const string EatingOut = "Eating Out";
         public const string Other = "Other";
 
+        private static int _length = -1;
         public static int Length
         {
-            get { return Members().Length; }
+            get
+            {
+                if (_length == -1)
+                {
+                    _length = Members.Count();
+                }
+                return _length;
+            }
+        }
+
+        private static IEnumerable<string> _members;
+        public static IEnumerable<string> Members
+        {
+            get
+            {
+                if (_members == null)
+                {
+                    _members = typeof (Categories).GetFields(BindingFlags.Static | BindingFlags.Public)
+                        .Select(field => field.GetValue(null).ToString());
+                }
+                return _members;
+            }
         }
 
         public static string Get(int index)
         {
-            return Members()[index];
-        }
-
-        public static string[] Members()
-        {
-            return new[] {Rent, PhoneBill, Entertainment, Grocery, Transportation, EatingOut, Other};
+            return Members.ElementAt(index);
         }
     }
 }
