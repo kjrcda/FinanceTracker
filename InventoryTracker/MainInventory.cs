d806fd113c176be8645b545f5d4fbac19bf3cdf6
kjrcda <kjrcda@hotmail.com>
2016-10-03 23:34:02 -0500

    Add blank inventory tracker project

---
+++ InventoryTracker/MainInventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace InventoryTracker
+{
+    public partial class InventoryTracker : Form
+    {
+        public InventoryTracker()
+        {
+            InitializeComponent();
+        }
+    }
+}
