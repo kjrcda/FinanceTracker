831f86557832990e8556fc68083b17b0de689249
kjrcda <kjrcda@hotmail.com>
2022-10-06 23:29:46 -0500

    Create month object and extend archive month

--- FinanceTracker/DataObjects/ArchiveMonth.cs
+++ FinanceTracker/DataObjects/ArchiveMonth.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace FinanceTracker.DataObjects
 {
-    public class ArchiveMonth
+    public class ArchiveMonth : Month
     {
-        public String MonthName="";
-        public List<double> MonthProj = new List<double>();
-        public List<FinanceEntry> MonthInfo = new List<FinanceEntry>();
         public double MonthProjTotal;
         public double MonthInfoTotal;
 
         public ArchiveMonth() { }
 
-        public ArchiveMonth(String name, List<double> proj, double projTotal, List<FinanceEntry> list, double listTotal)
+        public ArchiveMonth(string name, List<double> proj, double projTotal, List<FinanceEntry> list, double listTotal)
+            : base(name, proj, list)
         {
-            MonthName = name;
-            MonthProj = proj;
             MonthProjTotal = projTotal;
-            MonthInfo = list;
             MonthInfoTotal = listTotal;
         }
     }
---
+++ FinanceTracker/DataObjects/Month.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinanceTracker.DataObjects
+{
+    public class Month
+    {
+        public Guid Identifier = new();
+        public string MonthName = "";
+        public List<double> MonthProj = new();
+        public List<FinanceEntry> MonthInfo = new();
+
+        public Month() { }
+
+        public Month(string name, List<double> proj, List<FinanceEntry> list)
+        {
+            MonthName = name;
+            MonthProj = proj;
+            MonthInfo = list;
+        }
+    }
+}
