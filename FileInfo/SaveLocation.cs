16cfa5eea7006fbc787159250311da372e754844
kjrcda <kjrcda@hotmail.com>
2022-10-06 23:54:16 -0500

    Define temp location for save files

--- FileInfo/SaveLocation.cs
+++ FileInfo/SaveLocation.cs
@@ -1,10 +1,18 @@
-﻿using System;
-
-namespace FileInfo
+﻿namespace FileInfo
 {
     public static class SaveLocation
     {
         public static string FolderName { get; } = "FinanceTracker";
+
+#if DEBUG
+        public static string Path {
+            get {
+                var currentLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                return System.IO.Path.Combine(System.IO.Path.GetDirectoryName(currentLocation), FolderName);
+            }
+        }
+#else
         public static string Path { get; } = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), FolderName);
+#endif
     }
 }
