7304481825cc3e6d551cdb450fbbd7c5afc70bf6
kjrcda <kjrcda@hotmail.com>
2022-10-13 00:25:01 -0500

    Change to use local location as default

--- FileInfo/SaveLocation.cs
+++ FileInfo/SaveLocation.cs
@@ -4,15 +4,17 @@
     {
         public static string FolderName { get; } = "FinanceTracker";
 
-#if DEBUG
-        public static string Path {
-            get {
+#if USESYSTEMPATH
+        public static string Path { get; } = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), FolderName);
+#else
+        public static string Path
+        {
+            get
+            {
                 var currentLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
                 return System.IO.Path.Combine(System.IO.Path.GetDirectoryName(currentLocation), FolderName);
             }
         }
-#else
-        public static string Path { get; } = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), FolderName);
 #endif
     }
 }
